6f19ea77de4bc85d7eac49adf80d314ca3feccec
swassi <j_santana8@icloud.com>
2022-02-04 12:32:19 -0800

    inital commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+alert('index.js');
+import getDateString from './date.js';
+document
+    .querySelector('.js-content')
+    .innerHTML = `It's ${ getDateString() }`;
+    import getDateString from './date.js';
+import '../styles/styles.scss';
+document
+    .querySelector('.js-content')
+    .innerHTML = `It's ${ getDateString() }`;
